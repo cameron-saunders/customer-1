83b39c431cfa1c4ff2d2175b137145e6fbabe844
Dr. Qureshi <anique.qureshi@qc.cuny.edu>
2025-05-05 19:00:30 -0400

    added form tag

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -2,11 +2,13 @@ export default function Page() {
     return (
       <>
         <h1>Add Customer</h1>
-        <label htmlFor="fname">First Name</label>
-        <input id='fname' type="text" name='first_name'/>
-        <label htmlFor="lname">Last Name</label>
-        <input id='lname' type="text" name='last_name'/>
-        <button>Save</button>
+        <form>
+          <label htmlFor="fname">First Name</label>
+          <input id='fname' type="text" name='first_name'/>
+          <label htmlFor="lname">Last Name</label>
+          <input id='lname' type="text" name='last_name'/>
+          <button>Save</button>
+        </form>
       </>
     );
   }
